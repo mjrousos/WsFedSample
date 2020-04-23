759a50ebec1671d9249ea1d3103fc5ea9c45d489
Mike Rousos <Mike@MJRousos.com>
2020-04-23 12:09:42 -0400

    Initial relying party implementation

---
+++ RelyingParty/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+
+namespace RelyingParty
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+    class Calculator : ICalculator
+    {
+        public double Add(double x, double y) => x + y;
+
+        public double Divide(double x, double y) => x / y;
+
+        public double Multiply(double x, double y) => x * y;
+
+        public double Subtract(double x, double y) => x - y;
+    }
+}
--- RelyingParty/Program.cs
+++ RelyingParty/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Serilog;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Security;
 
 namespace RelyingParty
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            ConfigureLogging();
+
+            using (var host = new ServiceHost(typeof(Calculator)))
+            {
+                // For demo purposes, just load the key from disk so that no one needs to install an untrustworthy self-signed cert
+                var certPath = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "RelyingParty.pfx");
+                host.Credentials.ServiceCertificate.Certificate = new X509Certificate2(certPath, "[CertPassword]");
+                host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.None;
+                host.Open();
+                Log.Information("Calculator Service listening");
+
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+                Log.Information("Shutting down...");
+
+                host.Close();
+            }
+        }
+
+        private static void ConfigureLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("Logging initialized");
         }
     }
 }
