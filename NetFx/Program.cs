ff15d8c3b579a832a93043bf1d6fb3050d0dc790
Mike Rousos <Mike@MJRousos.com>
2020-04-23 13:46:12 -0400

    Add initial WCF client implementation

--- NetFx/Program.cs
+++ NetFx/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NetFx
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
+            var calculatorClient = new CalculatorServiceClient();
+            calculatorClient.ClientCredentials.UserName.UserName = "username";
+            calculatorClient.ClientCredentials.UserName.Password = "password";
+
+            Console.WriteLine($"11 * 5 = {await calculatorClient.MultiplyAsync(11, 5)}");
         }
     }
 }
